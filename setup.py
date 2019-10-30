104d0b22d96c9dad83ff047d36645dd001ede6a0
Tomáš Jozífek <jozifektomas@gmail.com>
2019-10-30 11:32:09 +0100

    manifest bad

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README') as f:
 
 setuptools.setup(
     name='ghia_joziftom',
-    version='0.3.2',
+    version='0.3.3',
     description='Assigns people to issues based on config files',
     long_description=long_description,
     long_description_content_type="text/plain",
